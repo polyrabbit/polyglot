62a8bb2f210a7aa439c6035eb14b0ddb1c3c7dbb
poly <mcx_221@foxmail.com>
2015-05-05 23:10:24 +0800

    Add test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,37 @@
 import unittest
+import os
+from polyglot.model import LanguageModel
+from polyglot.classifier import Classifier
+import cStringIO
+
+import logging
+logging.basicConfig(format='%(asctime)s -- %(message)s', level=logging.DEBUG)
 
 class TestShit(unittest.TestCase):
-    def test_universe(self):
-        self.assertTrue(1+1==2)
+    ngram = 3
+    corpus_dir = './corpus'
+    model_fp = cStringIO.StringIO()
+
+    def train(self):
+        print 'Training'
+        db = LanguageModel(self.model_fp)
+        master = Classifier(db, self.ngram)
+        master.train(self.corpus_dir)
+        print 'Train done'
+
+    def test_corpus(self):
+        self.train()
+
+        _, langs, _ = os.walk(self.corpus_dir).next()
+        for lang in filter(lambda l: not l.startswith('.'), langs):
+            file_paths = []
+            for root, _, files in os.walk(os.path.join(self.corpus_dir, lang)):
+                file_paths.extend(map(lambda fn: os.path.join(root, fn), files))
+            for fpath in file_paths:
+                self.model_fp.seek(0)
+                db = LanguageModel(self.model_fp)
+                master = Classifier(db, self.ngram)
+                self.assertEqual(master.classify(open(fpath).read())[0][0], lang)
 
 if __name__ == '__main__':
     unittest.main()
